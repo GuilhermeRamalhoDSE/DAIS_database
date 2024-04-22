3d0d22eeb85dd0b3fb4c7f28d258980d38585e5f
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-04-22 14:12:21 +0200

    Update contributionia.service.js

---
+++ frontend/src/app/contributionsIA/contributionia.service.js
@@ -0,0 +1,32 @@
+angular.module('frontend').factory('ContributioniaService', ['$http', function($http) {
+    var service = {};
+    var baseUrl = 'http://127.0.0.1:8000/api/contributionsia/';
+
+    service.getAll = function(layerId) {
+        return $http.get(baseUrl, { params: { layer_id: layerId } });
+    };
+
+    service.getById = function(contributionId) {
+        return $http.get(baseUrl + contributionId);
+    };
+
+    service.create = function(contributionData) {
+        return $http.post(baseUrl, contributionData, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        });
+    };
+
+    service.update = function(contributionId, contributionData) {
+        return $http.put(baseUrl + contributionId, contributionData, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        });
+    };
+
+    service.delete = function(contributionId) {
+        return $http.delete(baseUrl + contributionId);
+    };
+
+    return service;
+}]);
