0d7f5b1b97114a4364e96a37b1cd7d8c4155fae9
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-04-23 11:26:21 +0200

    Create formation_schema.py

---
+++ dais/schemas/formation_schema.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+from pydantic import BaseModel, Field, validator
+from typing import Optional, List
+from django.db.models.fields.files import FieldFile
+
+class LayerOut(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+class LanguageOut(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+class VoiceOut(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+class FormationCreateSchema(BaseModel):
+    layer_id: int
+    name: str
+    file_path: Optional[str] = Field(None, alias='file')
+    voice_id: int
+    language_id: int
+    trigger: str
+
+class FormationUpdateSchema(BaseModel):
+    layer_id: Optional[int] = None
+    name: Optional[str] = None
+    file_path: Optional[str] = Field(None, alias='file')
+    voice_id: Optional[int] = None
+    language_id: Optional[int] = None
+    trigger: Optional[str] = None
+
+class FormationSchema(BaseModel):
+    id: int
+    name: str
+    file_path: Optional[str] = Field(None, alias='file')
+    layer: LayerOut
+    voice: VoiceOut
+    languege: LanguageOut
+    trigger: str
+    last_update_date: datetime
+    created_at: datetime
+
+    @validator('file_path', pre=True, always=True)
+    def convert_fiel_to_url(cls,v):
+        if isinstance(v, FieldFile) and v.name:
+            return v.url
+        return v
+    
+    class Config:
+        from_attributes = True
