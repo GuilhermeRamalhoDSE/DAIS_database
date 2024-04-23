bcf3839bbf0ba4fa81a8e96f23c04237e2aab0c4
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-04-23 14:41:31 +0200

    Update formation.service.js

---
+++ frontend/src/app/formations/formation.service.js
@@ -0,0 +1,32 @@
+angular.module('frontend').factory('FormationService', ['$http', function($http){
+    var service = {};
+    var baseUrl = 'http://127.0.0.1:8000/api/formations/'
+
+    service.getAll = function(layerId) {
+        return $http.get(baseUrl, { params: { layer_id:layerId}});
+    };
+
+    service.getById = function(formationId) {
+        return $http.get(baseUrl + formationId);
+    };
+
+    service.create = function(formationData) {
+        return $http.post(baseUrl, formationData, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        });
+    };
+
+    service.update = function(formationId, formationData) {
+        return $http.put(baseUrl + formationId, formationData, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        });
+    };
+
+    service.delete = function(formationId) {
+        return $http.delete(baseUrl + formationId);
+    };
+
+    return service;
+}])
