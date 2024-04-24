73833f6246791c5e7d9fbcbd9172bf0b4bafa5fe
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-04-24 17:26:59 +0200

    Update license_route.py

--- dais/routes/license_route.py
+++ dais/routes/license_route.py
@@ -18,7 +18,7 @@ license_router = Router(tags=['Licenses'])
 
 @license_router.post("/", response={201: LicenseSchema}, auth=[QueryTokenAuth(), HeaderTokenAuth()])
 def create_license(request, payload: LicenseCreateSchema):
-    license_data = payload.dict(exclude={'avatars_id', 'voices_id', 'languages_id', 'module_id'})
+    license_data = payload.dict(exclude={'avatars_id', 'voices_id', 'languages_id', 'modules_id'})
     license_obj = License.objects.create(**license_data)
     
     return LicenseSchema.from_orm(license_obj)
