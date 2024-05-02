af7c5ef556f859235ab5014853058b76bbe4e57c
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-05-02 15:43:31 +0200

    Adjustment for deployment

--- frontend/src/app/screentype/screentype.service.js
+++ frontend/src/app/screentype/screentype.service.js
@@ -1,6 +1,6 @@
 angular.module('frontend').factory('ScreenTypeService', ['$http', function($http) {
     var service = {};
-    var baseUrl = 'http://127.0.0.1:8000/api/screentypes/'
+    var baseUrl = 'https://daisdatabasedse.it/api/screentypes/'
 
     service.create = function(screentypeData) {
         return $http.post(baseUrl, screentypeData);
