339284dff2ff21d7ff312fd94980e0297e2a8eed
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-05-03 12:46:59 +0200

    Update clientmodule.service.js

---
+++ frontend/src/app/client_module/clientmodule.service.js
@@ -0,0 +1,26 @@
+angular.module('frontend').factory('ClientModuleService', ['$http', function($http){
+    var service = {};
+    var baseUrl = 'http://localhost:8000/api/clientmodules/'
+
+    service.getAll = function(clientId) {
+        return $http.get(baseUrl, { params: { client_id:clientId}});
+    };
+
+    service.getById = function(clientmoduleId) {
+        return $http.get(baseUrl + clientmoduleId);
+    };
+
+    service.create = function(clientmoduleData) {
+        return $http.post(baseUrl, clientmoduleData)
+    };
+
+    service.update = function(clientmoduleId, clientmoduleData) {
+        return $http.put(baseUrl + clientmoduleId, clientmoduleData);
+    };
+
+    service.delete = function(clientmoduleId) {
+        return $http.delete(baseUrl + clientmoduleId);
+    };
+
+    return service;
+}])
