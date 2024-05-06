18f685ed7cbc21000647821002bec4fa29c06729
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-05-06 15:47:23 +0200

    Create formfield_models.py

---
+++ dais/models/formfield_models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from dais.models.form_models import Form
+
+class FormField(models.Model):
+    FIELD_TYPES = [
+        ('Text', _('Testo')),
+        ('Number', _('Numero')),
+        ('Date', _('Data'))
+    ]
+    form = models.ForeignKey(Form, on_delete=models.CASCADE, verbose_name=_('Form'))
+    name = models.CharField(max_length=255, verbose_name=_('Nome'))
+    numero = models.IntegerField(verbose_name=_('Numero'))
+    field_type = models.CharField(max_length=50, choices=FIELD_TYPES, default='text', verbose_name=_('Tipo di Campo'))
+    required = models.BooleanField(default=False, verbose_name=_('Obbligatorio'))
+
+    class Meta:
+        verbose_name = _('Campo di Form')
+        verbose_name_plural = _('Campi di Form')
+        ordering = ['numero']
+
+    def __self__(self):
+        return f'{self.numero} - {self.name}' 
