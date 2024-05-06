8fcdaecfd6a7f8dcca05b2216bc0506c18d57696
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-05-06 17:27:58 +0200

    Update form_field.service.js

---
+++ frontend/src/app/form_field/form_field.service.js
@@ -0,0 +1,26 @@
+angular.module('frontend').factory('FormFieldService', ['$http', function($http){
+    var service = {};
+    var baseUrl = 'http://localhost:8000/api/formfield/'
+
+    service.getAll = function(formId) {
+        return $http.get(baseUrl, { params: { form_id:formId}});
+    };
+
+    service.getById = function(formfieldId) {
+        return $http.get(baseUrl + formfieldId);
+    };
+
+    service.create = function(formfieldData) {
+        return $http.post(baseUrl, formfieldData);
+    };
+
+    service.update = function(formfieldId, formfieldData) {
+        return $http.put(baseUrl + formfieldId, formfieldData);
+    };
+
+    service.delete = function(formfieldId) {
+        return $http.delete(baseUrl + formfieldId);
+    };
+
+    return service;
+}])
