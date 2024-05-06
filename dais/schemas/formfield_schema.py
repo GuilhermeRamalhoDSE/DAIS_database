c8ebd7af0cc335da38dd3fd6d06a68d88ee2522d
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-05-06 15:58:24 +0200

    Create formfield_schema.py

---
+++ dais/schemas/formfield_schema.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class FormFieldCreateSchema(BaseModel):
+    form_id: int
+    name: str
+    numero: int
+    field_type: str
+    required: bool = False
+
+class FormFieldUpdateSchema(BaseModel):
+    form_id: Optional[int] = None
+    name: Optional[str] = None
+    numero: Optional[int] = None
+    field_type: Optional[str] = None
+    required: Optional[bool] = None
+
+class FormFieldSchema(BaseModel):
+    id: int
+    form_id: int
+    name: str
+    numero: int
+    field_type: str
+    required: bool
+
+    class Config:
+        from_attributes = True
+
