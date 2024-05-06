d448f4af56c5cbd33a811b2d38c694dd93a1e3b2
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-05-06 12:26:59 +0200

    Update form.update.controller.js

---
+++ frontend/src/app/forms/form.update.controller.js
@@ -0,0 +1,62 @@
+angular.module('frontend').controller('FormUpdateController', ['$scope', 'FormService','AuthService', '$state', '$stateParams', function($scope, FormService, AuthService, $state, $stateParams) {
+
+    $scope.isSuperuser = AuthService.isSuperuser();
+    $scope.clientId = $stateParams.clientId;
+    $scope.clientName = $stateParams.clientName;
+    $scope.clientmoduleId = $stateParams.clientmoduleId;
+    $scope.formId = $stateParams.formId;
+    $scope.formName = $stateParams.formName;
+    
+    $scope.formData = {};
+
+    $scope.loadFormDetails = function() {
+        if(!$scope.formId){
+            console.log('No form ID provided.');
+            alert('No form ID provided.')
+            $state.go('base.form-view', { 
+                clientId: $scope.clientId,
+                clientName: $scope.clientName,
+                clientmoduleId: $scope.clientmoduleId,
+             });
+             return;
+        }
+    FormService.getById($scope.formId).then(function(response) {
+        if (response.data) {
+            $scope.formData = response.data;
+            } else {
+                console.error('Form not found');
+                alert('Form not found.');
+                $state.go('base.form-view', { 
+                    clientId: $scope.clientId,
+                    clientName: $scope.clientName,
+                    clientmoduleId: $scope.clientmoduleId,
+                 });
+            }
+        }).catch(function(error) {
+            console.error('Error fetching form details:', error);
+        });
+    }; 
+
+    $scope.updateFormation = function() {
+        FormService.update($scope.formId, formData).then(function(response) {
+            alert('Form updated successfully!');
+            $state.go('base.form-view', { 
+                clientId: $scope.clientId,
+                clientName: $scope.clientName,
+                clientmoduleId: $scope.clientmoduleId,
+             });
+        }).catch(function(error) {
+            console.error('Error updating form:', error);
+        });
+    };
+
+    $scope.cancelUpdate = function() {
+        $state.go('base.form-view', { 
+            clientId: $scope.clientId,
+            clientName: $scope.clientName,
+            clientmoduleId: $scope.clientmoduleId,
+         });
+    };
+
+    $scope.loadFormDetails();
+}]);
