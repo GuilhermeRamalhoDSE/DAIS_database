f61d1ebaeaec8de2f7b5ad6ee8c81e784ef13ec5
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-05-06 10:43:24 +0200

    form service

--- dais/schemas/form_schema.py
+++ dais/schemas/form_schema.py
@@ -4,20 +4,17 @@ from typing import Optional
 
 class FormCreateSchema(BaseModel):
     client_module_id: int
-    module_id: int
     name: str
     api: bool
 
 class FormUpdateSchema(BaseModel):
     client_module_id: Optional[int] = None
-    module_id: Optional[int] = None
     name: Optional[str] = None
     api: Optional[bool] = None
 
 class FormSchema(BaseModel):
     id: int
     client_module_id: int
-    module_id: int
     name: str
     api: bool
     last_update: datetime
---
+++ frontend/src/app/forms/form.service.js
@@ -0,0 +1,26 @@
+angular.module('frontend').factory('FormService', ['$http', function($http){
+    var service = {};
+    var baseUrl = 'http://localhost:8000/api/forms/'
+
+    service.getAll = function(clientmoduleId) {
+        return $http.get(baseUrl, { params: { client_module_id:clientmoduleId}});
+    };
+
+    service.getById = function(formId) {
+        return $http.get(baseUrl + formId);
+    };
+
+    service.create = function(formData) {
+        return $http.post(baseUrl, formData);
+    };
+
+    service.update = function(formId, formData) {
+        return $http.put(baseUrl + formId, formData);
+    };
+
+    service.delete = function(formId) {
+        return $http.delete(baseUrl + formId);
+    };
+
+    return service;
+}])
