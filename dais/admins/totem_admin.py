114efb3ac65d8b4087aae6abd8b4ca031ef13da8
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-05-08 09:58:18 +0200

    Update totem_admin.py

--- dais/admins/totem_admin.py
+++ dais/admins/totem_admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from dais.models.totem_models import Totem
 
 class TotemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'group', 'installation_date', 'active', 'screen_count')
-    list_filter = ('active', 'installation_date', 'group')
+    list_display = ('id', 'name', 'group', 'installation_date', 'active', 'screen_count', 'last_update')
+    list_filter = ('active', 'installation_date', 'group', 'last_update')
     search_fields = ("name", 'group__name')
     date_hierarchy = 'installation_date'
     ordering = ('installation_date',)
     readonly_fields = ('screen_count',) 
-    fields = ('name', 'group', 'installation_date', 'active', 'comments', 'screen_count')
+    fields = ('name', 'group', 'installation_date', 'active', 'comments', 'screen_count', 'last_update')
 
     def screen_count(self, obj):
         return obj.screen_count
