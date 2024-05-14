c7eeba18f5333a7157d9c2eebc6bfa08d940b673
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-05-14 16:32:19 +0200

    Update touchscreen_interactions_admin.py

--- dais/admins/touchscreen_interactions_admin.py
+++ dais/admins/touchscreen_interactions_admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from dais.models.touchscreen_interactions_models import TouchScreenInteractions
 
 class TouchScreenInteractionsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'clientmodule_id', 'name', 'total_button', 'last_update')
-    search_fields = ('name',)
-    list_filter = ('name', 'id', 'last_update')
+    list_display = ('id', 'clientmodule_id', 'name', 'total_buttons', 'last_update')
+    search_fields = ('id','name')
+    list_filter = ('id','name')
+    ordering = ('id',)
+    fields = ('id', 'client_module', 'name', 'total_buttons', 'last_update')
+    readonly_fields = ('last_update',)
 
     def total_buttons(self, obj):
         return obj.total_buttons
