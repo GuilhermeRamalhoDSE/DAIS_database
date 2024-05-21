8f8e5243523301bd68cc0bf8bba344a85737df46
Guilherme Ramalho <guilherme.ramalho@gruppodse.com>
2024-05-21 10:53:47 +0200

    Update buttontype.controller.js

---
+++ frontend/src/app/buttontype/buttontype.controller.js
@@ -0,0 +1,57 @@
+angular.module('frontend').controller('ButtonTypeController', ['$scope', 'ButtonTypeService', '$state', 'AuthService', function($scope, ButtonTypeService, $state, AuthService) {
+    $scope.buttontypes = [];
+    $scope.newButtonType = {
+        name: "",
+    }
+    
+    $scope.loadButtonTypes = function() {
+        ButtonTypeService.getAll().then(function(response){
+            $scope.buttontypes = response.data;
+        }, function(error) {
+            console.error('Error loading button type:', error);
+        });
+    };
+
+    $scope.goToNewButtonType = function() {
+        $state.go('base.buttontype-new');
+    };
+
+    $scope.createButtonType = function() {
+        ButtonTypeService.create($scope.newButtonType).then(function(response) {
+            alert('Button Type created successfully!');
+            $state.go('base.buttontype-view'); 
+        }).catch(function(error) {
+            console.error('Error creating button type:', error);
+        });
+    };
+
+    $scope.editButtonType = function(buttontypeId) {
+        $state.go('base.buttontype-update', { buttontypeId: buttontypeId });
+    };
+
+    $scope.cancelCreate = function() {
+        $state.go('base.buttontype-view');
+    };
+
+    $scope.goBack = function() {
+        $state.go('base.home');
+    };
+
+    $scope.deleteButtonType = function(buttontypeId) {
+        if (confirm('Are you sure you want to delete this button type?')) {
+            ButtonTypeService.delete(buttontypeId).then(function(response) {
+                alert('Button Type deleted successfully!');
+                $scope.loadButtonTypes();
+                $state.go('base.buttontype-view'); 
+            }).catch(function(error) {
+                console.error('Error deleting button type:', error);
+            });
+        }
+    };
+
+    $scope.isSuperuser = function() {
+        return AuthService.isSuperuser();
+    };
+
+    $scope.loadButtonTypes();
+}]);
