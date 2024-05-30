1e114b940fa78b445215008d55b6e5ef42081b30
Guilherme Ramalho <161315764+GuilhermeRamalhoDSE@users.noreply.github.com>
2024-05-30 14:59:20 +0200

    Client update: Now only the name is mandatory, and you can also see the status field, which we couldn't see before

--- dais/schemas/client_schema.py
+++ dais/schemas/client_schema.py
@@ -1,12 +1,13 @@
 from ninja import Schema
 from typing import Optional
 
+
 class ClientIn(Schema):
     name: str
-    email: str
-    address: str
-    phone: str
-    active: bool = True  
+    email: Optional[str] = None
+    address: Optional[str] = None
+    phone: Optional[str] = None
+    active: Optional[bool] = True
 
 
 class ClientOut(Schema):
