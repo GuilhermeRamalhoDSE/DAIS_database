6da1df7968f1e5ee0c7c072fb9100fb3d70d870f
Guilherme Ramalho <161315764+GuilhermeRamalhoDSE@users.noreply.github.com>
2024-06-24 17:47:05 +0200

    Visual adjustments in logs

--- frontend/src/app/logs/logs.controller.js
+++ frontend/src/app/logs/logs.controller.js
@@ -4,6 +4,55 @@ angular.module('frontend').controller('LogsController', ['$scope', 'LogsService'
     $scope.from = new Date();
     $scope.from.setMonth($scope.from.getMonth()-1);
     $scope.to = new Date();
+    $scope.currentPage = 0;
+    $scope.pageSize = 5;
+
+    $scope.getPaginatedData = function() {
+        var filteredList = $filter('filter')($scope.logs, $scope.searchText);
+        filteredList = $filter('dateFilter')(filteredList, $scope.from, $scope.to);
+        filteredList = $filter('filter')(filteredList, {
+            date: $scope.dateSearchText,
+            client: $scope.clientSearchText,
+            campaign: $scope.campaignSearchText,
+            typology: $scope.typologySearchText,
+            logtype: $scope.logtypeSearchText,
+            information: $scope.informationSearchText
+        });
+        filteredList = $filter('customTotemIdFilter')(filteredList, $scope.totemIdSearchText);
+
+        var startIndex = $scope.currentPage * $scope.pageSize;
+        var endIndex = Math.min(startIndex + $scope.pageSize, filteredList.length);
+        return filteredList.slice(startIndex, endIndex);
+    };
+
+    $scope.setCurrentPage = function(page) {
+        if (page >= 0 && page < $scope.totalPages()) {
+            $scope.currentPage = page;
+        }
+    };
+
+    $scope.totalPages = function() {
+        var filteredList = $filter('filter')($scope.logs, $scope.searchText);
+        filteredList = $filter('dateFilter')(filteredList, $scope.from, $scope.to);
+        filteredList = $filter('filter')(filteredList, {
+            date: $scope.dateSearchText,
+            client: $scope.clientSearchText,
+            campaign: $scope.campaignSearchText,
+            typology: $scope.typologySearchText,
+            logtype: $scope.logtypeSearchText,
+            information: $scope.informationSearchText
+        });
+        filteredList = $filter('customTotemIdFilter')(filteredList, $scope.totemIdSearchText);
+        return Math.ceil(filteredList.length / $scope.pageSize);
+    };
+
+    $scope.getPages = function() {
+        var pages = [];
+        for (var i = 0; i < $scope.totalPages(); i++) {
+            pages.push(i);
+        }
+        return pages;
+    };
 
     $scope.loadLogs = function() {
         var licenseId = AuthService.getLicenseId();
@@ -15,7 +64,7 @@ angular.module('frontend').controller('LogsController', ['$scope', 'LogsService'
         }).catch(function(error) {
             console.error('Error loading logs:', error);
         });
-    };    
+    };
 
     $scope.deleteLog = function(logId) {
         if (!logId) {
@@ -34,7 +83,6 @@ angular.module('frontend').controller('LogsController', ['$scope', 'LogsService'
 
     $scope.exportToCSV = function() {
         var csvContent = "data:text/csv;charset=utf-8,";
-
         var headers = ["Date", "Totem ID", "Client", "Campaign", "Totem Type", "Log Type", "Information"];
         csvContent += headers.join(",") + "\n";
 
@@ -56,7 +104,6 @@ angular.module('frontend').controller('LogsController', ['$scope', 'LogsService'
         link.setAttribute("href", encodedUri);
         link.setAttribute("download", "logs.csv");
         document.body.appendChild(link);
-
         link.click();
     };
 
