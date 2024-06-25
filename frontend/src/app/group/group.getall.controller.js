a78c6b8fe50f05bc89ad99e65adc3b40260c2698
Guilherme Ramalho <161315764+GuilhermeRamalhoDSE@users.noreply.github.com>
2024-06-25 17:30:35 +0200

    Update group.getall.controller.js

--- frontend/src/app/group/group.getall.controller.js
+++ frontend/src/app/group/group.getall.controller.js
@@ -3,7 +3,8 @@ angular.module('frontend').controller('GroupGetAllController', ['$scope', '$stat
     $scope.moduleFormEnabled = false;
     $scope.modulesAvailable = AuthService.hasModules();
     $scope.currentPage = 0;
-    $scope.pageSize = 5;
+    $scope.pageSize = 10;
+    $scope.visiblePages = 3;
 
     $scope.checkModuleFormEnabled = function() {
         if (AuthService.isModuleEnabled('form')) {
@@ -33,9 +34,28 @@ angular.module('frontend').controller('GroupGetAllController', ['$scope', '$stat
     
     $scope.getPages = function() {
         var pages = [];
-        for (var i = 0; i < $scope.totalPages(); i++) {
+        var total = $scope.totalPages();
+        var startPage = Math.max(0, $scope.currentPage - Math.floor($scope.visiblePages / 2));
+        var endPage = Math.min(total, startPage + $scope.visiblePages);
+    
+        if (startPage > 0) {
+            pages.push(0);
+            if (startPage > 1) {
+                pages.push('...');
+            }
+        }
+    
+        for (var i = startPage; i < endPage; i++) {
             pages.push(i);
         }
+    
+        if (endPage < total) {
+            if (endPage < total - 1) {
+                pages.push('...');
+            }
+            pages.push(total - 1);
+        }
+    
         return pages;
     };
 
