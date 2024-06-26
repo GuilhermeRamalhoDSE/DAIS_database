b0d51848b2a63120533ab3729acf327121c0a726
Guilherme Ramalho <161315764+GuilhermeRamalhoDSE@users.noreply.github.com>
2024-06-26 17:11:15 +0200

    Adjustment in the pagination bar for screen type

--- frontend/src/app/screentype/screentype.controller.js
+++ frontend/src/app/screentype/screentype.controller.js
@@ -1,7 +1,8 @@
 angular.module('frontend').controller('ScreenTypeController', ['$scope', 'ScreenTypeService', '$state', 'AuthService', '$filter', function($scope, ScreenTypeService, $state, AuthService, $filter) {
     $scope.screentypes = [];
     $scope.currentPage = 0;
-    $scope.pageSize = 5;
+    $scope.pageSize = 10;
+    $scope.visiblePages = 3;
     $scope.newScreenType = {
         name: "",
     };
@@ -26,9 +27,28 @@ angular.module('frontend').controller('ScreenTypeController', ['$scope', 'Screen
     
     $scope.getPages = function() {
         var pages = [];
-        for (var i = 0; i < $scope.totalPages(); i++) {
+        var total = $scope.totalPages();
+        var startPage = Math.max(0, $scope.currentPage - Math.floor($scope.visiblePages / 2));
+        var endPage = Math.min(total, startPage + $scope.visiblePages);
+    
+        if (startPage > 0) {
+            pages.push(0);
+            if (startPage > 1) {
+                pages.push('...');
+            }
+        }
+    
+        for (var i = startPage; i < endPage; i++) {
             pages.push(i);
         }
+    
+        if (endPage < total) {
+            if (endPage < total - 1) {
+                pages.push('...');
+            }
+            pages.push(total - 1);
+        }
+    
         return pages;
     };
     
